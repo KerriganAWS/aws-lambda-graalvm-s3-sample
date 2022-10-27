fce37dec7c217e09be1ea2358839d6b91d36f6aa
Lin <yenhlin@88665a0e13fc.ant.amazon.com>
2022-10-27 19:50:47 +0800

    changed codes

--- build-native-image.sh
+++ build-native-image.sh
@@ -2,6 +2,6 @@
 
 ./mvnw clean install -P native-image
 
-aws s3 cp lambda-native.zip s3://lambda-upload-testing/artifacts/
+aws s3 cp ./target/lambda-native.zip s3://lambda-upload-testing/artifacts/
 
 aws lambda update-function-code --function-name s3-put-http-testing-function --s3-bucket lambda-upload-testing --s3-key artifacts/lambda-native.zip
